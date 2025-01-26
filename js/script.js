99c02d4b2afd012bec14a0b073f8820ce92020f0
Giovanni <187939419+GI028@users.noreply.github.com>
2025-01-26 09:30:45 +0800

    creation of drop down and showing them with overlay on small screen

--- js/script.js
+++ js/script.js
@@ -49,55 +49,95 @@ $(document).ready(function(){
         scrolling = false;
     });
 
-    const swiper = new Swiper('.swiper', {
-        loop: false,
-        slidesPerView:3,
-        spaceBetween:15,
-        centeredSlides:true,
-        effect: 'coverflow',
-        coverflowEffect: {
-            rotate: 30,
-            slideShadows: true,
-        },
-        breakpoints: {
-            // when window width is >= 480px
-            0: {
-                slidesPerView: 1,
-                initialSlide: 0,
+    try {
+        const swiper = new Swiper('.swiper', {
+            loop: false,
+            slidesPerView:3,
+            spaceBetween:15,
+            centeredSlides:true,
+            effect: 'coverflow',
+            coverflowEffect: {
+                rotate: 30,
+                slideShadows: true,
             },
-            480: {
-                slidesPerView: 2,
-                initialSlide: 1,
+            breakpoints: {
+                // when window width is >= 480px
+                0: {
+                    slidesPerView: 1,
+                    initialSlide: 0,
+                },
+                480: {
+                    slidesPerView: 2,
+                    initialSlide: 1,
+                },
+                768: { 
+                    slidesPerView: 3,
+                    initialSlide: 1,
+                },
+                1000: {
+                    slidesPerView: 5,
+                    initialSlide: 2,
+                }
+              },
+            initialSlide: 2,
+            autoplay: {
+              delay: 3000,
+              disableOnInteraction: false,
             },
-            768: { 
-                slidesPerView: 3,
-                initialSlide: 1,
+            // Pagination
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+                dynamicBullets:true,
+                dynamicMainBullets:2
             },
-            1000: {
-                slidesPerView: 5,
-                initialSlide: 2,
-            }
-          },
-        initialSlide: 2,
-        autoplay: {
-          delay: 3000,
-          disableOnInteraction: false,
-        },
-        // Pagination
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-            dynamicBullets:true,
-            dynamicMainBullets:2
-        },
-        mousewheel:{
-            enabled:true,
-            forceToAxis:true,
-        },
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      });
+            mousewheel:{
+                enabled:true,
+                forceToAxis:true,
+            },
+            // Navigation arrows
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+        }); 
+    } catch (error) {
+        //do something
+    }
+
+    let overlay = $('.overlay');
+    if(overlay.length === 0){
+        $('body').prepend(`
+            <div class="overlay hidden">
+                <div>
+                    <button class="close">
+                        <i class="fa-solid fa-xmark"></i>
+                    </button>
+                    <div class="content">
+                    </div>
+                </div>
+            </div>
+        `);
+        overlay = $('.overlay');
+    }
+
+    $(".dropdown-btn").click(function(e){
+        e.preventDefault();
+        const screenWidth = parseInt($( document ).width());        
+        if(screenWidth < 768){
+            const dropdown = $(this).next();
+            const container = overlay.find('.content')
+            overlay.removeClass('hidden');
+            container.html('');
+            container.append(dropdown.clone());
+        }
+    });
+    $(".overlay .close").click(function(e){
+        $(this).parent().parent().addClass('hidden');
+    })
+    $(".overlay").click(function(e){
+        if(e.target == this)
+            $(this).addClass('hidden');
+    });
+
 });
